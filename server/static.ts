98c166cd5a07d732025fd0b6f6937973b439c67d
Messiri4 <messirimiracle@gmail.com>
2026-01-09 20:44:32 -0800

    Fix static file serving path for production

--- server/static.ts
+++ server/static.ts
@@ -3,15 +3,33 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  // In production, files are in dist/public relative to process.cwd()
+  const distPath = path.resolve(process.cwd(), "dist/public");
+  
+  console.log("Looking for static files at:", distPath);
+  console.log("Does it exist?", fs.existsSync(distPath));
+  
   if (!fs.existsSync(distPath)) {
+    // Try alternative path (in case __dirname works)
+    const altPath = path.resolve(__dirname, "public");
+    console.log("Trying alternative path:", altPath);
+    console.log("Does it exist?", fs.existsSync(altPath));
+    
+    if (fs.existsSync(altPath)) {
+      app.use(express.static(altPath));
+      app.use("*", (_req, res) => {
+        res.sendFile(path.resolve(altPath, "index.html"));
+      });
+      return;
+    }
+    
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
-
+  
   app.use(express.static(distPath));
-
+  
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
